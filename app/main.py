c7b5fee06f3e8f6ccf3ba1eb3065ede59f139e14
Mike <tradewcs@gmail.com>
2025-09-26 09:38:23 +0300

    rounded init value of average_rating

--- app/main.py
+++ app/main.py
@@ -20,15 +20,16 @@ class CarWashStation:
 
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
-        self.average_rating = average_rating
+        self.average_rating = round(average_rating, 1)
         self.count_of_ratings = count_of_ratings
 
     def calculate_washing_price(self, car: Car) -> float:
-        return (
+        return round(
             (self.clean_power - car.clean_mark)
             * car.comfort_class
             * self.average_rating
-            / self.distance_from_city_center
+            / self.distance_from_city_center,
+            1
         )
 
     def serve_cars(self, cars: list[Car]) -> float:
